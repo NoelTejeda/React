9238136d8f1817a630abebcac6a9eb0ec078871f
Noel <noelati@gmail.com>
2023-06-25 11:26:59 -0400

    se agrega loading

--- fetch_Pro/src/App.jsx
+++ fetch_Pro/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import { useFetch } from './useFetch'
+import PropagateLoader
+from "react-spinners/PropagateLoader";
 
 function App() {
   const {data, loading}=useFetch('https://jsonplaceholder.typicode.com/users')
@@ -9,7 +11,19 @@ function App() {
        <div className="card">
        <ul>
          {/* evaluea a true primeramente, sino hay nada, no muestra nada */}
-         {loading && <li>loading...</li> }
+         {/* {loading && <li>loading...</li> } */}
+         {loading && (
+  <li>
+    <PropagateLoader
+      color={'#36d7b7'}
+      loading={loading}
+      
+      size={30}
+      aria-label="Loading Spinner"
+      data-testid="loader"
+    />
+  </li>
+)}
          {/* si existe data y si existe queremos que atrevez de map itere de usuarios y devuelva un elemento li */}
          {data?.map((user)=>(<li key={user.id}>{user.name}</li>))}
        </ul>
@@ -20,3 +34,8 @@ function App() {
  }
 
 export default App
+
+
+/* para agregar el loading:
+* https://www.npmjs.com/package/react-spinners
+* https://www.davidhu.io/react-spinners/storybook/?path=/docs/propagateloader--main */
